126c896e3670c36f9f0a856504eb2d9638ad6046
MarceloIgnacio8 <144186256+MarceloIgnacio8@users.noreply.github.com>
2023-12-08 10:42:00 -0300

    Update main.dart

--- Actividad1/main.dart
+++ Actividad1/main.dart
@@ -1,1 +1,26 @@
+// lib/main.dart
+
+import 'package:flutter/material.dart';
+import 'widget.dart'; 
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('App de Flutter'),
+        ),
+        body: Center(
+         
+          child: MiWidget(),
+        ),
+      ),
+    );
+  }
+}
 
